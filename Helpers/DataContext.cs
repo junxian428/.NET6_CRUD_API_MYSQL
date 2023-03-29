42a4b3d03c9adc1956c0742fdd81d0773f0eb724
junxian428 <holeyiho@gmail.com>
2023-03-29 14:04:03 +0800

    first commit

--- Helpers/DataContext.cs
+++ Helpers/DataContext.cs
@@ -15,7 +15,9 @@ public class DataContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
         // in memory database used for simplicity, change to a real db for production applications
-        options.UseInMemoryDatabase("TestDb");
+        //options.UseInMemoryDatabase("TestDb");
+        var connectionString = Configuration.GetConnectionString("WebApiDatabase");
+        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
     }
 
     public DbSet<User> Users { get; set; }
